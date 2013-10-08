8b29054da5c4b0f34518898719db83d84afa7eb9
p <pino.silvaggio@gmail.com>
2013-10-07 21:30:17 -0400

    start

---
+++ src/main/java/org/bitstrings/maven/plugins/properties/PropertiesMojo.java
@@ -0,0 +1,11 @@
+package org.bitstrings.maven.plugins.properties;
+
+import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_RESOURCES;
+
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo( name = "compose", defaultPhase = GENERATE_RESOURCES, threadSafe = true )
+public class PropertiesMojo
+{
+
+}
